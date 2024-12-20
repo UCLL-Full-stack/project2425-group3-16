a2bda24f754a5a961d3539dc04a162fd52f4141a
MartheDHoogheUCLL <marthe.dhooghe@student.ucll.be>
2024-12-20 20:06:27 +0100

    signup userservice

--- front-end/services/UserService.ts
+++ front-end/services/UserService.ts
@@ -10,8 +10,18 @@ const loginUser = (user: User) => {
     });
 };
 
+const signupUser = (user: User) => {
+    return fetch(process.env.NEXT_PUBLIC_API_URL + `/users/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        }, body: JSON.stringify({ user })
+    });
+}
+
 const UserService = {
     loginUser,
+    signupUser
 };
 
 export default UserService;
