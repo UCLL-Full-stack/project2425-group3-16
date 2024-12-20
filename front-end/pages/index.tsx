baa78a9091feda44776748ea442703173741a57d
MartheDHoogheUCLL <marthe.dhooghe@student.ucll.be>
2024-12-20 23:32:22 +0100

    credentials table

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -8,30 +8,53 @@ const Home: React.FC = () => {
         <>
             <Head>
                 <title>RecipeShare</title>
-                <meta name="description" content="Courses app"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <meta name="description" content="Courses app" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Header/>
+            <Header />
             <main className={styles.main}>
-        <span>
-          <Image
-              src="/images/recipes.png"
-              alt="Courses Logo"
-              className={styles.vercelLogo}
-              width={50}
-              height={50}
-          />
-          <h1>Welcome!</h1>
-        </span>
+                <span>
+                    <Image
+                        src="/images/recipes.png"
+                        alt="Courses Logo"
+                        className={styles.vercelLogo}
+                        width={50}
+                        height={50}
+                    />
+                    <h1>Welcome!</h1>
+                </span>
 
                 <div className={styles.description}>
                     <p>
-                        RecipeShare lets you discover and share recipes with a community of food enthusiasts. <br/>
-                        As a user, you can browse, save, and share your favorite recipes. <br/>
+                        RecipeShare lets you discover and share recipes with a community of food enthusiasts. <br />
+                        As a user, you can browse, save, and share your favorite recipes. <br />
                         You can also see detailed instructions, ingredients, and user reviews for each recipe.
                     </p>
                 </div>
+                <h2>Credentials</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>User</td>
+                            <td>user</td>
+                        </tr>
+                        <tr>
+                            <td>Chef</td>
+                            <td>chef</td>
+                        </tr>
+                        <tr>
+                            <td>Admin</td>
+                            <td>admin</td>
+                        </tr>
+                    </tbody>
+                </table>
             </main>
         </>
     )
